70d1ef02d9da8d98676a84ebbbc74abc97fa79bc
son426 <coghks0426@naver.com>
2025-08-21 15:40:30 +0900

    shadcn 세팅

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,13 @@
+import Button from "@/components/ui/button";
 import { Link } from "react-router";
 
 const HomePage = () => {
   return (
     <div className="bg-red-200 w-full h-500">
       <h1>보일러 플레이트지롱</h1>
-
+      <Button variant="secondary" onClick={() => console.log("!!")}>
+        테스트
+      </Button>
       <nav>
         <Link to="/about">About</Link>
       </nav>
